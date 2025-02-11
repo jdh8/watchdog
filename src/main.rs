9f3f6e574f9e7fdd312b57577af62314250e9432
Chen-Pang He <chen.pang.he@jdh8.org>
2025-02-12 06:54:09 +0800

    Try cache invalidation

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 use dotenv::var;
 use serde_json::json;
 
-async fn ping(client: &reqwest::Client) -> anyhow::Result<()> {
-    const ENDPOINT: &str = "https://natsuki-oehk.shuttle.app/";
-    anyhow::ensure!(client.head(ENDPOINT).send().await?.status().is_success());
-    Ok(())
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let client = reqwest::Client::new();
-
-    if let Err(error) = ping(&client).await {
+    let ping = || async {
+        const ENDPOINT: &str = "https://natsuki-oehk.shuttle.app/";
+        anyhow::ensure!(client.head(ENDPOINT).send().await?.status().is_success());
+        Ok(())
+    };
+    if let Err(error) = ping().await {
         client
             .post(var("WEBHOOK")?)
             .json(&json!({ "content": format!("{}: {error:#?}", var("MENTION")?) }))
             .send()
             .await?;
     }
-
     Ok(())
 }
